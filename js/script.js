a272095ccc77c9cc626bae2083d55181e34c3b42
Karen os <karenestelaos@gmail.com>
2016-11-30 01:27:22 -0500

    estilos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(e) {
     $('img[usemap]').rwdImageMaps();
+    //scroll
+    $(function() {
+       $('nav a').click(function() {
+           if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+               var target = $(this.hash);
+               target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+               if (target.length) {
+                   $('html,body').animate({
+                       scrollTop: target.offset().top
+                   }, 1000);
+                   return false;
+               }
+           }
+       });
+   });
 });
